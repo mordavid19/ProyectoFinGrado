0d6b5a373fbdc920a9c414324ab2a30b4f456dc3
mordavid19 <daid.belmonte.moreno@gmail.com>
2025-06-19 13:02:13 +0200

    Solucionado errores y mas

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-  include 'cabeceras-piePagina/Arriba.php';
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -11,7 +8,16 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 </head>
 <body>
-
+<header class="header">
+    <h1 class="logo"><a href="index.php" style="text-decoration: none; color: inherit;">FitnessPro</a></h1>
+    <nav class="nav">
+      <div class="hamburger">☰</div>
+      <div class="nav-links">
+        <a href="index.php" class="nav-link">Inicio</a>
+        <a href="Publico/login.php" class="nav-link">Ya eres socio</a>
+      </div>
+    </nav>
+  </header>
 
   <main class="main-content1">
     <h2>Bienvenido a FitnessPro</h2>
